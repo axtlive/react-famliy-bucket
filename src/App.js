8925a552c09e60c0e940967fde8b5b74458e281b
axtlive <axtlive@foxmail.com>
2020-09-21 11:36:34 +0800

    useEffect-pratice

--- src/App.js
+++ src/App.js
@@ -1,37 +1,8 @@
-import React, { useEffect, useState } from "react";
-
-let n = 0;
-
-function fun1() {
-  console.log("fun1的副作用函数");
-  return () => {
-    console.log("fun1的清理函数");
-  };
-}
-
-function fun2() {
-  console.log("fun2的副作用函数");
-  return () => {
-    console.log("fun2的清理函数");
-  };
-}
+import React from "react";
+import Admin from './pages/Admin'
 
 export default function App() {
-  const [, forceUpdate] = useState({});
-
-  useEffect(n % 2 === 0 ? fun1 : fun2);
-
-  n++;
-
   return (
-    <div>
-      <button
-        onClick={() => {
-          forceUpdate({});
-        }}
-      >
-        +
-      </button>
-    </div>
+    <Admin />
   );
 }
